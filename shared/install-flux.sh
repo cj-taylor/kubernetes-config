39f9da32ae9212cc17b1bc5483c7abe147751a54
Matt Moriarity <matt@mattmoriarity.com>
2018-12-04 08:58:18 -0600

    Revert worker job limiter changes.

--- shared/install-flux.sh
+++ shared/install-flux.sh
@@ -2,19 +2,12 @@
 
 NAMESPACE=$(kubectl config current-context)
 
-BRANCH="master"
-if [[ $NAMESPACE == *staging* ]]; then
-  BRANCH="staging"
-fi
-
 helm repo add weaveworks https://weaveworks.github.io/flux
 helm upgrade flux weaveworks/flux \
-  --version 0.4.0 \
   --install \
   --set rbac.create=true \
   --set helmOperator.create=true \
   --set git.url=ssh://git@github.com/travis-ci/kubernetes-config.git \
-  --set "git.branch=$BRANCH" \
   --set "git.path=releases/$NAMESPACE" \
   --set git.chartsPath=charts \
   --set git.pollInterval=1m \
